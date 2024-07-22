e95a259f643889d65d1c4272f0903f4fbc096a19
UjjvalBihola <ujjvalrbihola@gmail.com>
2024-07-22 06:55:25 +0530

    Modify drive ride component test file

--- frontend/src/components/main/__test__/DriveRide.test.js
+++ frontend/src/components/main/__test__/DriveRide.test.js
@@ -1,36 +1,35 @@
 import Enzyme, { shallow } from "enzyme";
 import React from "react";
-import Adapter from 'enzyme-adapter-react-16';
-import Cookies from 'js-cookie';
+import Adapter from "enzyme-adapter-react-16";
+import Cookies from "js-cookie";
 import DriveRide from "../DriveRide";
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("drive", () => {
+  let wrapper;
 
-    let wrapper;
+  beforeEach(() => {
+    Cookies.get = jest.fn().mockImplementation(() => "tokken");
+    wrapper = shallow(<DriveRide type={"drive"} />);
+  });
 
-    beforeEach(() => {
-        Cookies.get = jest.fn().mockImplementation(() => 'tokken');
-        wrapper = shallow(<DriveRide type={'drive'} />);
-    });
+  it("Should render source button", () => {
+    const sourceButton = wrapper.find(`[data-test='source-button']`);
+    expect(sourceButton.length).toBe(1);
+  });
 
-    it("Should render source button", () => {
-        const sourceButton= wrapper.find(`[data-test='source-button']`);
-        expect(sourceButton.length).toBe(1);
-     });
+  it("Should render destination button", () => {
+    const destinationButton = wrapper.find(`[data-test='destination-button']`);
+    expect(destinationButton.length).toBe(1);
+  });
 
-     it("Should render destination button", () => {
-        const destinationButton= wrapper.find(`[data-test='destination-button']`);
-        expect(destinationButton.length).toBe(1);
-     });
+  it("Should render drive button", () => {
+    const driveButton = wrapper.find(`[data-test='drive-submit-button']`);
+    expect(driveButton.length).toBe(1);
+  });
 
-     it("Should render drive button", () => {
-        const driveButton= wrapper.find(`[data-test='drive-submit-button']`);
-        expect(driveButton.length).toBe(1);
-     });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-})
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
