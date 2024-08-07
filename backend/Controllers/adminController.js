5f96d0392d89c4725e38cc719efe524efe213de3
UjjvalBihola <ujjvalrbihola@gmail.com>
2024-08-07 09:50:30 +0530

    Code refactor of admin controller

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -1,22 +1,22 @@
-const Trip = require("../Models/tripModel"); // Assuming Trip model is defined
-const User = require("../Models/user"); // Assuming User model is defined
+const Trip = require("../Models/tripModel");
+const User = require("../Models/user");
 
 exports.getAllTrips = async (req, res) => {
-    try {
-        const trips = await Trip.find();
-        res.json(trips);
-    } catch (error) {
-        console.error("Error fetching trips:", error);
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    const trips = await Trip.find();
+    res.json(trips);
+  } catch (error) {
+    console.error("Error fetching trips:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(500).json({ message: "Server error" });
-    }
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
