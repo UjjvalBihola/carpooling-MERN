6f03fc86cc816d0bee103fc11351476fbb701e33
UjjvalBihola <ujjvalrbihola@gmail.com>
2024-08-12 12:50:12 +0530

    Code refactor of all users route path

--- backend/Controllers/allusersRoutes.js
+++ backend/Controllers/allusersRoutes.js
@@ -1,14 +1,6 @@
-//import  u from "../Models/user.js";
-const u = require("../Models/user.js")
+const user = require("../Models/user.js");
 exports.allusersRoutes = (req, res) => {
-    u.find().exec((err, ud) => {
-        // if(err){
-        //     res.status(400).json({
-        //         error:"no user found"
-        //     })
-        // }
-        res.json(ud)
-    })
-}
-
-//export {allusersRoutes}
+  user.find().exec((err, ud) => {
+    res.json(ud);
+  });
+};
