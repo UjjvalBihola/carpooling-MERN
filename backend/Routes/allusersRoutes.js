cb16a5239cff4fb456f34786227615cf125e2f8d
UjjvalBihola <ujjvalrbihola@gmail.com>
2024-12-25 06:30:58 +0530

    Removed unused code of authentication file

--- backend/Routes/allusersRoutes.js
+++ backend/Routes/allusersRoutes.js
@@ -1,10 +1,7 @@
 /* temporary route to get all the user information*/
-//import express from "express";
 const express = require("express");
-//import {allusersRoutes} from "../Controllers/allusersRoutes.js";
 const { allusersRoutes } = require("../Controllers/allusersRoutes.js");
-var router = express.Router()
+var router = express.Router();
 
-router.get("/users", allusersRoutes)
+router.get("/users", allusersRoutes);
 module.exports = router;
-//export default router;
