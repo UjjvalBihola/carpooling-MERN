1951af7962d16d9791e4e3c58444bb383475515f
UjjvalBihola <ujjvalrbihola@gmail.com>
2024-12-29 16:46:13 +0530

    Update indent of remaining library files

--- frontend/src/libraries/UseAdminToken.js
+++ frontend/src/libraries/UseAdminToken.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function useAdminToken() {
-    // Retrieve the token from localStorage
-    const getToken = () => {
-        return localStorage.getItem('adminToken');
-    };
+  // Retrieve the token from localStorage
+  const getToken = () => {
+    return localStorage.getItem("adminToken");
+  };
 
-    const [token, setToken] = useState(getToken());
+  const [token, setToken] = useState(getToken());
 
-    // Save token to localStorage and update state
-    const saveToken = (userToken) => {
-        localStorage.setItem('adminToken', userToken);
-        setToken(userToken);
-    };
+  // Save token to localStorage and update state
+  const saveToken = (userToken) => {
+    localStorage.setItem("adminToken", userToken);
+    setToken(userToken);
+  };
 
-    return {
-        setToken: saveToken,
-        token
-    };
+  return {
+    setToken: saveToken,
+    token,
+  };
 }
 
 export default useAdminToken;
